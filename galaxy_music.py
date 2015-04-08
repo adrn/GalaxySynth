fd45b336841768ca7bb94bf29737d10ac3b215d0
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-04-08 09:56:26 -0400

    basslines and highlines

--- galaxy_music.py
+++ galaxy_music.py
@@ -71,17 +71,16 @@ def make_anim(w, ix, ntrails=25):
         return pts,  # ,trails
 
     anim = FuncAnimation(fig, animate,
-                         frames=100, interval=50)  # , blit=True)
-    # anim.save("/Users/adrian/Downloads/derp.mp4")
-    plt.show()
-    return
+                         frames=10, interval=1000)  # , blit=True)
+
     return anim
 
 def main():
     np.random.seed(42)
 
-    norbits = 4
+    norbits = 8
     nsteps = 10000
+    dt = 2.5
 
     # axisymmetric potential
     # pot = gp.LogarithmicPotential(v_c=1., r_h=0.1, q1=1., q2=1., q3=0.6,
@@ -106,13 +105,17 @@ def main():
     w0 = np.hstack((xyz0,vxyz0))
 
     # integrate orbits
-    t,w = pot.integrate_orbit(w0, dt=2.5, nsteps=nsteps)
+    t,w = pot.integrate_orbit(w0, dt=dt, nsteps=nsteps)
     R = np.sqrt(w[:,:,0]**2 + w[:,:,1]**2)
     phi = np.arctan2(w[:,:,1], w[:,:,0])
     z = w[:,:,2]
 
     # Animate the orbits
-    # make_anim(w, [0,1], ntrails=10)
+    # anim_xy = make_anim(w, [0,1], ntrails=10)
+    # anim_xy.save("/Users/adrian/projects/galaxy-synthesizer/output/xy.mp4")
+
+    # anim_xz = make_anim(w, [0,2], ntrails=10)
+    # anim_xz.save("/Users/adrian/projects/galaxy-synthesizer/output/xz.mp4", bitrate=-1)
 
     # find when orbits cross
     # estimate periods -- find longest and shortest periods
@@ -137,18 +140,22 @@ def main():
         return q
 
     # variable length arrays
-    phi_cross = np.array([argrelmin(pphi**2)[0] for pphi in phi.T])
+    phi_cross = np.array([argrelmin(pphi)[0] for pphi in phi.T])
     z_cross = np.array([argrelmin(zz**2)[0] for zz in z.T])
 
-    s = pyo.Server().boot()
+    s = pyo.Server(audio="offline", nchnls=2, sr=44100).boot()
+    s.recordOptions(dur=20.,
+                    filename="/Users/adrian/projects/galaxy-synthesizer/output/le_test.wav",
+                    fileformat=0)
 
     # define a scale object
-    mode = 'ionian'
-    high_freqs = musak.Scale(key='C', mode=mode, octave=4).freqs
+    mode = 'aeolian'
+    key = 'C'
+    high_freqs = musak.Scale(key=key, mode=mode, octave=4).freqs
     high_freqs = np.append(high_freqs, high_freqs*2.)
     high_freqs = np.append(high_freqs, high_freqs*2.)
 
-    low_freqs = musak.Scale(key='C', mode=mode, octave=3).freqs
+    low_freqs = musak.Scale(key=key, mode=mode, octave=2).freqs
     low_freqs = np.append(low_freqs, low_freqs*2.)
     low_freqs = np.append(low_freqs, low_freqs*2.)
 
@@ -159,32 +166,53 @@ def main():
     _cache = []
     # for j in range(1,nsteps):
     for j in range(1,100):
-        j_freqs = []
+        delay = t[j] / dt / 4.
+        print(t[j], delay)
+
+        hif = []
+        lof = []
         for k in range(norbits):
             if j in z_cross[k]:
-                j_freqs.append(high_freqs[q_R[j,k]])
+                hif.append(high_freqs[q_R[j,k]])
 
-            elif j in phi_cross[k]:
-                j_freqs.append(low_freqs[q_R[j,k]])
+            if j in phi_cross[k]:
+                print('phi cross', j)
+                lof.append(low_freqs[q_R[j,k]])
 
-        if len(j_freqs) == 0:
-            continue
+        if len(hif) > 0:
+            env = pyo.Fader(fadein=tone_dur*0.02, fadeout=tone_dur*0.02, dur=tone_dur*0.9, mul=0.01).play(delay=delay, dur=tone_dur+0.1)
+            osc = pyo.Sine(freq=hif, mul=env).mix(voices=norbits)
+            osc.out(delay=delay, dur=tone_dur)
 
-        delay = t[j]/5.
-        print(t[j], delay)
-        env = pyo.Fader(fadein=tone_dur*0.02, fadeout=tone_dur*0.02, dur=tone_dur*0.9, mul=0.1).play(delay=delay, dur=tone_dur+0.1)
-        osc = pyo.Sine(freq=j_freqs, mul=env).mix(voices=norbits)
-        verb = pyo.Freeverb(osc, size=0.5, damp=0.5, bal=1., add=0).out(delay=delay, dur=tone_dur)
-        osc.out(delay=delay, dur=tone_dur)
+            _cache.append(osc)
+            _cache.append(env)
 
-        _cache.append(osc)
-        _cache.append(env)
+        if len(lof) > 0:
+            lo_env = pyo.Fader(fadein=tone_dur*0.02, fadeout=tone_dur*0.02, dur=tone_dur*0.9, mul=0.01).play(delay=delay, dur=tone_dur+0.1)
+            lo_osc = pyo.Sine(freq=lof, mul=lo_env).mix(voices=norbits)
+            # lo_osc.out(delay=delay, dur=tone_dur)
+            lo_chorus = pyo.Chorus(lo_osc).out(delay=delay, dur=tone_dur*1.1)
 
+            _cache.append(lo_osc)
+            _cache.append(lo_env)
+            _cache.append(lo_chorus)
+
+        # wave = pyo.SquareTable(order=15).normalize()
+        # osc = pyo.Osc(table=wave, freq=j_freqs, mul=env).mix(voices=norbits)
+        # verb = pyo.Freeverb(osc, size=1., damp=0.8, bal=0.85).out(delay=delay, dur=tone_dur*2)
+        # chorus = pyo.Chorus(osc).out(delay=delay, dur=tone_dur*2.)
+
+        # _cache.append(wave)
+        # _cache.append(chorus)
+        # _cache.append(verb)
+
+    # print(t[phi_cross[0][0]])
+
+    s.recstart()
     s.start()
+    s.recstop()
     s.stop()
-
-    s.gui(locals())
-
+    # s.gui(locals())
 
 if __name__ == "__main__":
     main()
